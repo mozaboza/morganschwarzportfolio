99a7e5213bf866d74538789f4d7bf9fdb8add028
mozaboza <mozaboza@outlook.com>
2024-12-03 04:23:28 -0700

    Add files via upload

---
+++ SchwarzM_WEB1_Project/script.js
@@ -0,0 +1,14 @@
+function openLightbox(imageSrc, captionText) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const lightboxCaption = document.getElementById('lightbox-caption');
+
+    lightboxImg.src = `images/${imageSrc}`;
+    lightboxCaption.textContent = captionText;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
